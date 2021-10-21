6cf0713c44a6dd98a327b169928c289df1b74c14
kimkj1108 <kimkj1108@gmail.com>
2021-10-21 20:23:36 +0900

    디버그 어려워...

--- lab201-ioc/src/main/java/egovframework/lab/ioc/service/impl/AnnotationEmpServiceImpl.java
+++ lab201-ioc/src/main/java/egovframework/lab/ioc/service/impl/AnnotationEmpServiceImpl.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
 import egovframework.lab.ioc.postprocess.Debug;
 import egovframework.lab.ioc.service.EmpService;
 import egovframework.lab.ioc.service.EmpVO;
 
+@Service("annotationEmpService")
 public class AnnotationEmpServiceImpl implements EmpService {
 
     // TODO [Step 2-3] AnnotationEmpServiceImpl 작성
--- lab201-ioc/src/test/java/egovframework/lab/ioc/service/EmpServiceTest.java
+++ lab201-ioc/src/test/java/egovframework/lab/ioc/service/EmpServiceTest.java
@@ -13,6 +13,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//이곳에는 테스트 케이스를 작성 합니다.
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
