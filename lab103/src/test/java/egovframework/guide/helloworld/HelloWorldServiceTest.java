b6a5f2e608545d722b9f087ac19f5c3191dc34b4
kimkj1108 <kimkj1108@gmail.com>
2021-10-29 10:44:03 +0900

    study

--- lab103/src/test/java/egovframework/guide/helloworld/HelloWorldServiceTest.java
+++ lab103/src/test/java/egovframework/guide/helloworld/HelloWorldServiceTest.java
@@ -12,9 +12,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/context-helloworld.xml" })//참조 할 빈에 대한...
 public class HelloWorldServiceTest{
+	
 	private HelloWorldService helloworld;
 	
-	@Resource(name = "helloworld")//helloworld을 어노테이션으로 주입 받는다.
+	@Resource(name = "helloworld")//"helloworld" 빈을 어노테이션으로 주입 받는다.
 	public void setHelloWorld(HelloWorldService hello) {
 		this.helloworld = hello;
 	}
